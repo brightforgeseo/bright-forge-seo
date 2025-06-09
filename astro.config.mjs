5328058886c1e74ecdcd70a240f17a713d8eea4a
Your Name <your.email@example.com>
2025-06-09 16:39:27 +0800

    feat: add minimal sitemap configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brightforgeseo.com',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap()
+  ],
   output: 'static',
   build: {
     inlineStylesheets: 'auto'
