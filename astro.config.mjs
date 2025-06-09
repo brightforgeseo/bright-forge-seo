59ea7ecda90640b607b0af5521fbd7d205c8a9b4
Your Name <your.email@example.com>
2025-06-09 16:44:31 +0800

    fix: simplify config to bare minimum

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://brightforgeseo.com',
   integrations: [tailwind()],
-  output: 'static',
-  build: {
-    inlineStylesheets: 'never'
-  }
+  output: 'static'
 });
